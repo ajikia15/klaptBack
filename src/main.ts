425da66ac50b785c206ba5090bb280204ae3c5e3
ajikia15 <aleksandrej15@gmail.com>
2025-04-02 17:27:41 +0400

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,10 @@ async function bootstrap() {
       transform: true,
     }),
   );
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
