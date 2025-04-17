b41c0d1f730380c9227f3488d86dd0d2db1db1f1
ajikia15 <aleksandrej15@gmail.com>
2025-04-17 21:01:40 +0400

    server available on local network for other devices

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,25 @@ const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(
     cookieSession({
       keys: ['asdfasdfasdf'],
     }),
   );
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
+
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://192.168.1.235:5173'],
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
